2b6e79977a88629ae572398e8dcd58765f185b58
moumou758 <moumou758@163.com>
2025-08-18 12:17:27 +0800

    Update values.py

--- selfdrive/car/tesla/values.py
+++ selfdrive/car/tesla/values.py
@@ -72,10 +72,10 @@ BUTTONS = [
 
 
 class CarControllerParams:
-  ANGLE_RATE_LIMIT_UP = AngleRateLimit(speed_bp=[0., 5., 25.], angle_v=[2.5, 1.5, 0.2])
-  ANGLE_RATE_LIMIT_DOWN = AngleRateLimit(speed_bp=[0., 5., 25.], angle_v=[5., 2.0, 0.3])
-  JERK_LIMIT_MAX = 4.9
-  JERK_LIMIT_MIN = -4.9
+  ANGLE_RATE_LIMIT_UP = AngleRateLimit(speed_bp=[0., 5., 15.], angle_v=[10., 1.6, .3])
+  ANGLE_RATE_LIMIT_DOWN = AngleRateLimit(speed_bp=[0., 5., 15.], angle_v=[10., 7.0, 0.8])
+  JERK_LIMIT_MAX = 8
+  JERK_LIMIT_MIN = -8
   ACCEL_TO_SPEED_MULTIPLIER = 3
   TORQUE_TO_ANGLE_MULTIPLIER_OUTER = 4  # Higher = easier to influence when manually steering more than OP
   TORQUE_TO_ANGLE_MULTIPLIER_INNER = 8  # Higher = easier to influence when manually steering less than OP
